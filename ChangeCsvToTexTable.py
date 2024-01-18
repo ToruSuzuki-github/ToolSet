fa526e3a09f06b3e5943c1025151d1eec471abd2
toruSurfaceLaptop4 <5519059s@st.toho-u.jp>
2024-01-18 11:20:48 +0900

    プログラムの更新

--- ChangeCsvToTexTable.py
+++ ChangeCsvToTexTable.py
@@ -1,46 +1,47 @@
+# オプション-fは未実装
+
 from sys import argv,stdout
 import os
 import csv
 from glob import glob
 
-def changeCsv(inputFilePath,outputFilePath):
-    if os.path.isfile(outputFilePath):
+def std_help(args):
+    print("Usage: Python3",args[0],"<converted file or dir path>","<output dir path>","[OPTION]...")
+    print("Create tex format table from csv file.")
+    print("-i, --interactive, When a file with the same name as the file to be output exists, select whether or not to overwrite the file one by one.")
+    #print("-f, --folder, All csv files in the folder are to be treated as the target of the letter.")
+    print("    --help, display this help and exit.")
+
+def check_file_exist(file_path):
+    if os.path.isfile(file_path):
         while True:
-            stdout.write('\n出力ファイル名と同じファイルが既に存在します。')
-            stdout.write('\n出力予定ファイルのファイルパス:\n'+str(outputFilePath))
-            stdout.write('\n上書きして大丈夫な場合『Yes』、処理を辞める場合『No』を入力してください。')
+            stdout.write('出力ファイル名と同じファイルが既に存在します。\n')
+            stdout.write('出力予定ファイルのファイルパス:\n'+str(file_path)+"\n")
+            stdout.write('上書きして大丈夫な場合『Y』、処理を辞める場合『N』を入力してください。\n')
             stdout.flush()
             try:
                 inputString=str(input())
             except ValueError:
-                stdout.write('\n入力内容が不正です。')
+                stdout.write('入力内容が不正です。\n')
                 stdout.flush()
                 continue
-            if inputString=='Yes':
-                break
-            elif inputString=='No':
+            if inputString=='Y':
+                return True
+            elif inputString=='N':
                 return False
             else:
-                stdout.write('入力内容が不正です。')
+                stdout.write('入力内容が不正です。\n')
                 stdout.flush()
                 continue
-        while True:
-            stdout.write('\n本当に上書きしてしまってかまいませんか?\n上書きして大丈夫な場合『Yes』、処理を辞める場合『No』を入力してください。')
-            stdout.flush()
-            try:
-                inputString=str(input())
-            except ValueError:
-                stdout.write('\n入力内容が不正です。')
-                stdout.flush()
-                continue
-            if inputString=='Yes':
-                break
-            elif inputString=='No':
-                return False
-            else:
-                continue
-    stdout.write('\n'+os.path.basename(inputFilePath)+' 処理中')
+
+def change_csv(inputFilePath,outputFilePath,i_flg):
+    
+    stdout.write('\n'+os.path.basename(inputFilePath))
     stdout.flush()
+    if i_flg:
+        if not check_file_exist(outputFilePath):
+            stdout.write(' quit\n')
+            return False
     with open(inputFilePath,'r') as rf:
         fileDataList=csv.reader(rf)
         with open(outputFilePath,'w') as wf:
@@ -55,59 +56,61 @@ def changeCsv(inputFilePath,outputFilePath):
                         wf.write(' \\\\\n')
                     else:
                         wf.write(' & ')
-    stdout.write('\n'+os.path.basename(inputFilePath)+' 処理完了')
-    stdout.write('\n出力ファイルのファイルパス:\n'+os.path.splitext(str(os.getcwd()))[0]+'/'+os.path.basename(outputFilePath))
+    stdout.write(' change\n')
+    stdout.write('output file: '+str(outputFilePath)+"\n")
     stdout.flush()
     return True
-if __name__=="__main__":
-    runFlag=False
-    helpFlag=False
-    args=argv
-    argument_list=["変換元ファイル/ディレクトリパス","変換後ファイル出力先ディレクトリパス"]
-    if ('-h' in args) or ('-help' in args):
-        stdout.write('\n【本プログラムの処理内容】')
-        stdout.write('\n指定したcsvファイルをtexファイルで表を表示するための表記に変更するプログラム。\n出力ファイルの形式は.txtファイル')
-        stdout.write('\n\n【本プログラムの実行形式】')
-        stdout.write("\nPython3 "+str(args[0]))
-        for argument in argument_list:
-            stdout.write(" <"+str(argument)+">")
-        helpFlag=True
-        result=True
-    elif len(args)==(len(argument_list)+1):
-        inputFilePath=args[1]
-        outputDirPath=args[2]
-        if os.path.dirname(outputDirPath):
-            try:
-                if os.path.isfile(inputFilePath):
-                    inputFilePathList=[inputFilePath]
-                    runFlag=True
-                elif os.path.isdir(inputFilePath):
-                    inputFilePathList=sorted(glob(inputFilePath+'/*.csv'))
-                    runFlag=True
-                else:
-                    stdout.write('\n変換元ファイルが存在しません')
-                    result=False
-            except FileNotFoundError:
-                stdout.write('\n変換元ファイルが存在しません')
-                result=False
+
+def main(args):
+    error=[]
+
+    # ヘルプ表示チェック
+    if len(args)==2:
+        # ヘルプ表示
+        if args[1]=="--help":
+            std_help(args)
+            return error
+    # 引数の数チェック
+    if len(args)<3 or 4<len(args):
+        error.append("引数の数が不正")
+        return error
+
+    # 出力先パスのチェック
+    if not os.path.isdir(args[2]):
+        error.append("出力先ディレクトリパスが不正")
+    
+    # 変換元パスのチェック
+    if os.path.isfile(args[1]):# 変換元がファイルパス
+        converted_files=[args[1]]
+    elif os.path.isdir(args[1]):# 変換元がディレクトリパス
+        converted_files=glob(args[1]+"/*.csv")
+    else:# 変換元が存在しない
+        error.append("変換元ファイルが存在しない")
+    
+    # オプションのチェック
+    i_flg=False
+    if len(args)==4:
+        options=args[3:]
+        if options[0] in {"-i", "--interactive"}:
+            i_flg=True
         else:
-            stdout.write("出力先ディレクトリが存在しません")
-            result=False
-    else:
-        result=False
-        stdout.write('\n引数の数が不正')
-    if runFlag:
-        for inputFilePath in inputFilePathList:
-            stdout.write('\n'+os.path.basename(inputFilePath)+' 処理開始')
-            stdout.flush()
-            outputFilePath=outputDirPath+"/"+os.path.splitext(os.path.basename(inputFilePath))[0]+'.tex.txt'
-            os.makedirs(os.path.dirname(outputFilePath),exist_ok=True)
-            result=changeCsv(inputFilePath,outputFilePath)
-            stdout.write('\n'+os.path.basename(inputFilePath)+' 処理終了')
-            stdout.flush()
-    if result or helpFlag:
-        stdout.write('\n正常終了')
-    else:
-        stdout.write('\n異常終了')
-    stdout.write('\n')
-    stdout.flush()
+            error.append("オプション指定の内容が不正")
+    
+    # 引数の内容が不正な場合
+    if len(error)>0:
+        return error
+    
+    # 実行
+    for converted_file in converted_files:
+        output_name=os.path.splitext(os.path.basename(converted_file))[0]+".tex"
+        change_csv(converted_file, args[2]+"/"+output_name, i_flg)
+    return error
+
+# -i	--interactive	削除前に確認する
+# "Usage: Python3",args[0],"<converted path>","<output dir path>","[OPTION]..."
+if __name__=="__main__":
+    error=main(argv)
+    if len(error)>0:
+        for error_detail in error:
+            print("FAILD:",error_detail)
+        print("Try \'python3 ",argv[0]," --help\' for more information.")
